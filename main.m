ec78b5776652beb65b11a68987dfb00ca6e94c14
caruemar <caruemar@alumni.uv.es>
2025-01-13 12:10:43 +0100

    Quitamos sample_rates y algo de Wiener.

--- main.m
+++ main.m
@@ -3,10 +3,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%
 
 % loading the dataset
-
 folder_path = "../DSD100subset/Mixtures/Dev";
-
 audio_files = dir(folder_path);
+sampling_rate = 44100;
 
 % this is to delete the directory . .. and .DS_files(compulsory in mac)
 % in windows i thing only needed . and .. so (3:end)
@@ -14,15 +13,13 @@ audio_files = audio_files(4:end);
 
 num_files = length(audio_files);
 audio_data = cell(num_files, 1);
-sample_rates = zeros(num_files, 1);
 
 for i = 1:num_files
     file_path = fullfile(folder_path, audio_files(i).name,'/mixture.wav');
-    [audio_data{i}, sample_rates(i)] = audioread(file_path);
+    audio_data{i} = audioread(file_path);
 end
 
 folder_path = "../DSD100subset/Sources/Dev";
-
 audio_files = dir(folder_path);
 
 % this is to delete the directory . .. and .DS_files(compulsory in mac)
@@ -31,7 +28,6 @@ audio_files = audio_files(3:end);
 
 num_files = length(audio_files);
 audio_data_raw = cell(num_files, 4);
-sample_rates_raw = zeros(num_files, 4);
 
 for i = 1:num_files
     file_path = fullfile(folder_path, audio_files(i).name);
@@ -42,33 +38,46 @@ for i = 1:num_files
     audio_files1 = audio_files1(3:end);
     for j = 1:length(audio_files1)
         file_path1 = fullfile(folder_path, audio_files(i).name,audio_files1(j).name);
-        [audio_data_raw{i,j}, sample_rates_raw(i,j)] = audioread(file_path1);
+        audio_data_raw{i,j} = audioread(file_path1);
     end
 end
 
+fprintf('Pre-Data sorting ended');
+
 %%
 
+%Wiener-Hopf
 % Generate example signals
+duration_seconds = 7;
 
 
 % x = audio_data{1}; % Input signal (e.g., noisy signal)
-x = audio_data_raw{1,4} + audio_data_raw{1,1}; % blending 2 audio files
+x = audio_data_raw{1,4} + audio_data_raw{1,2}; % blending 2 audio files
 x = x / max(abs(x(:))); % normalize to prevent clipping
 
 d = audio_data_raw{1,4}; % Desired signal
-
-sound(x, sample_rates(1));
-%pause(length(x)/sample_rates(1) + 1);
 
 % Apply Wiener-Hopf filter
 N = 64; % Filter order
 [w, y_est] = wienerHopf(x, d, N);
 [w, y_est] = wienerHopf(y_est, d, N); % if we do it a second time the results appear to be better
 
+%Hearing the blended original.
+fprintf('Playing the unfiltered blended one...\n');
+num_samples = duration_seconds * sample_rates(1);
+x_short = x(1:min(num_samples, length(x)));
+
+sound(x_short, sample_rates(1));
+pause(length(x_short) / sample_rates(1) + 1);
+
+%Hearing the filtered one.
+fprintf('Playing the filtered signal...\n');
+y_est_short = y_est(1:min(num_samples, length(y_est)));
+sound(y_est_short, sample_rates(1));
+pause(length(y_est_short) / sample_rates(1) + 1);
+
 n = 1:length(d);
 n1 = 1:length(y_est);
-
-sound(y_est, sample_rates(1));
 
 % Plot results
 figure;
